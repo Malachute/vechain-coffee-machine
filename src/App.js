dacc88455e0829a8383ab31f9ed1619da113317c
Mark <mhagethorn@gmail.com>
2021-09-01 16:38:51 +0200

    Public URL's

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
       <main className="content">
         <div className="container">
           <div className="row">
-            <div className="col-sm-12 col-md-3 col-lg-2"></div>
-            <div className="col-sm-12 col-md-5 col-lg-7"></div>
-            <div className="col-sm-12 col-md-4 col-lg-3"></div>
+            <div className="col-sm-12"></div>
           </div>
         </div>
       </main>
